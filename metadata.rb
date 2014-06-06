8921f6ccf251de1177a5695523b8a05537a357b6
Jeremy Olliver <jeremy.olliver@gmail.com>
2014-06-06 15:54:42 +1200

    Generate application cookbook with berkshelf 3.1.1

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'application'
+maintainer       'apachev2'
+maintainer_email 'jeremy.olliver@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures application'
+long_description 'Installs/Configures application'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: application
+# Recipe:: default
+#
+# Copyright (C) 2014 apachev2
+#
+# All rights reserved - Do Not Redistribute
+#
